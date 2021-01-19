06078f3c49726588942cec9f8354c059457fa590
Gordon Smith <GordonJSmith@gmail.com>
2021-01-19 04:45:12 +0000

    chore(build):  Script to purge jsdelivr cache
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

---
+++ utils/purge-jsdelivr.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const https = require("https");
+
+function get(id, url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, resp => {
+            let data = "";
+            resp.on("data", (chunk) => {
+                data += chunk;
+            });
+            resp.on("end", () => {
+                resolve(data);
+            });
+        }).on("error", (err) => {
+            reject(err);
+        });
+    });
+}
+
+async function purgePackage(id, ver) {
+    const purgeIndex = `https://purge.jsdelivr.net/npm/${id}${ver}/dist/index.js`;
+    const purgeIndexNode = `https://purge.jsdelivr.net/npm/${id}${ver}/dist/index.node.js`;
+    const purgeIndexMin = `https://purge.jsdelivr.net/npm/${id}${ver}/dist/index.min.js`;
+    const purgeIndexNodeMin = `https://purge.jsdelivr.net/npm/${id}${ver}/dist/index.node.min.js`;
+    const purgeGraphviz = `https://purge.jsdelivr.net/npm/${id}${ver}/dist/graphvizlib.wasm`;
+    const purgeExpat = `https://purge.jsdelivr.net/npm/${id}${ver}/dist/expatlib.wasm`;
+    const purgePackage = `https://purge.jsdelivr.net/npm/${id}${ver}/package.json`;
+    return Promise.all([
+        get(id, purgeIndex),
+        get(id, purgeIndexNode),
+        get(id, purgeIndexMin),
+        get(id, purgeIndexNodeMin),
+        get(id, purgeGraphviz),
+        get(id, purgeExpat),
+        get(id, purgePackage)
+    ]).then(responses => {
+        return {
+            id: `${id}${ver}`,
+            responses
+        };
+    });
+}
+
+async function purgeAll() {
+    await purgePackage("@hpcc-js/wasm", "")
+        .then(console.log)
+        .catch(e => console.error(e.message));
+}
+
+purgeAll();
