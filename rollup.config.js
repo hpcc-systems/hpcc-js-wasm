7c2c29441792a78db5fd7af8326072ae4cb6a6eb
Gordon Smith <GordonJSmith@gmail.com>
2021-08-11 14:58:25 +0100

    fix:  Rollback merging of NodeJS and Web builds
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,21 @@ import alias from "@rollup/plugin-alias";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import sourcemaps from "rollup-plugin-sourcemaps";
+import replace from "@rollup/plugin-replace";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require("./package.json");
-const globals = {
-    'fs': 'fs',
-    'crypto': 'crypto',
-    'path': 'path'
-};
-const bundleTpl = (input, umdOutput, esOutput) => ({
+const browserTpl = (input, umdOutput, esOutput) => ({
     input: input,
-    external: ["fs", "crypto", "path"],
     output: [{
         file: umdOutput,
         format: "umd",
         sourcemap: true,
-        name: pkg.name,
-        globals
+        name: pkg.name
     }, {
-        file: esOutput + ".js",
+        file: esOutput,
         format: "es",
-        sourcemap: true,
-        globals
+        sourcemap: true
     }],
     plugins: [
         alias({}),
@@ -34,7 +27,45 @@ const bundleTpl = (input, umdOutput, esOutput) => ({
         sourcemaps()
     ]
 });
+const nodeTpl = (input, cjsOutput, esOutput) => ({
+    input: input,
+    external: ["fs", "crypto", "path"],
+    output: [{
+        file: cjsOutput,
+        format: "cjs",
+        sourcemap: true,
+        name: pkg.name
+    }, {
+        file: esOutput,
+        format: "es",
+        sourcemap: true
+    }],
+    plugins: [
+        alias({
+            entries: [
+                { find: "../build/graphviz/graphvizlib/graphvizlib", replacement: "../build/graphviz/graphvizlib/graphvizlib.node" },
+                { find: "../build/expat/expatlib/expatlib", replacement: "../build/expat/expatlib/expatlib.node" }
+            ]
+        }),
+        replace({
+            preventAssignment: true,
+            ".node.wasm": ".wasm"
+        }),
+        nodeResolve({
+            preferBuiltins: true
+        }),
+        commonjs({}),
+        sourcemaps()
+    ]
+});
+
 export default [
-    bundleTpl("lib-es6/index", pkg.browser, pkg.module),
-    bundleTpl("lib-es6/__tests__/index", "dist/test.js", "dist/test.es6")
+    browserTpl("lib-es6/index", pkg.browser, pkg.module + ".js"),
+    browserTpl("lib-es6/graphviz", "dist/graphviz.js", "dist/graphviz.es6.js"),
+    browserTpl("lib-es6/expat", "dist/expat.js", "dist/expat.es6.js"),
+    nodeTpl("lib-es6/index", pkg.main, pkg["module-node"] + ".js"),
+    nodeTpl("lib-es6/graphviz", "dist/graphviz.node.js", "dist/graphviz.node.es6.js"),
+    nodeTpl("lib-es6/expat", "dist/expat.node.js", "dist/expat.node.es6.js"),
+    browserTpl("lib-es6/__tests__/index", "dist/test.js", "dist/test.es6.js"),
+    nodeTpl("lib-es6/__tests__/index", "dist/test.node.js", "dist/test.node.es6.js")
 ];
