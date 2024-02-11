5a737d91d3218f756c733441ce1dcd3b8adf9c02
Gordon Smith <GordonJSmith@gmail.com>
2024-02-11 09:16:18 +0000

    chore:  Bump Versions
    GraphViz 10.0.1
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- vcpkg-overlays/graphviz/portfile.cmake
+++ vcpkg-overlays/graphviz/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_gitlab(
   GITLAB_URL https://gitlab.com
   OUT_SOURCE_PATH SOURCE_PATH
   REPO graphviz/graphviz
-  REF 1cfb01454ca543f307c33bd8c34f20ee2004ddb6
-  SHA512 7ec8e8fd7ea0fece8f65d217cee84ea6081844a941716c8182ad5a8c5c95fc627cbef1e0164eecc322626247e8eb839cd6d4246e193c70ecb4d909bca49779bf
+  REF 1c6cb9d3de553bd3e3caeea9a61ebe04034d07ee
+  SHA512 8cee6e7b403c2516d4fd17b9806fa96b46e0791368b8411fa6245e5c30b680fc4e22c4ef59d6d56aaa0dfa449d356a59d4c712e250259ca049c0ceeb1f89b943
   HEAD_REF main
 )
 
@@ -12,6 +12,13 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -Denable_ltdl=OFF
+        -Dwith_gvedit=OFF
+        -Dwith_smyrna=OFF
+        -Dwith_zlib=OFF
+        -Duse_win_pre_inst_libs=OFF
+        -DBUILD_SHARED_LIBS=OFF
 )
 vcpkg_cmake_install()
 # vcpkg_cmake_config_fixup(PACKAGE_NAME "graphviz" CONFIG_PATH "share/cmake/graphviz")
