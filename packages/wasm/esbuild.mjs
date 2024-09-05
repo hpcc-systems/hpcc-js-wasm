2d41eeb89cc525e22658ca0a73e26e3da0aaae3a
Gordon Smith <GordonJSmith@gmail.com>
2024-09-05 16:52:54 +0100

    GH-257 Add missing DuckDB to wasm package
    Fixes #257
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/wasm/esbuild.mjs
+++ packages/wasm/esbuild.mjs
@@ -3,6 +3,7 @@ import { bothTpl, browserBoth, nodeBoth } from "@hpcc-js/esbuild-plugins";
 //  config  ---
 await Promise.all([
     bothTpl("src/base91.ts", "dist/base91", undefined, "@hpcc-js/wasm"),
+    bothTpl("src/duckdb.ts", "dist/duckdb", undefined, "@hpcc-js/wasm"),
     bothTpl("src/graphviz.ts", "dist/graphviz", undefined, "@hpcc-js/wasm"),
     bothTpl("src/expat.ts", "dist/expat", undefined, "@hpcc-js/wasm"),
     bothTpl("src/zstd.ts", "dist/zstd", undefined, "@hpcc-js/wasm")
