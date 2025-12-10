6cbd352e5ad97cd2a8c0b7c40c13c710eec27d18
Gordon Smith <GordonSmith@users.noreply.github.com>
2025-12-10 16:55:14 +0000

    fix: @hpcc-js/wasm does not include sub-package types (#344)
    fixes #340
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- packages/wasm/src/index.ts
+++ packages/wasm/src/index.ts
@@ -1,25 +1,31 @@
+import type * as Base91Types from "./base91.js";
+import type * as DuckDBTypes from "./duckdb.js";
+import type * as ExpatTypes from "./expat.js";
+import type * as GraphvizTypes from "./graphviz.js";
+import type * as ZstdTypes from "./zstd.js";
+
 export namespace Base91 {
-    export function load() {
+    export function load(): Promise<Base91Types.Base91> {
         return import("./base91.js").then(mod => mod.Base91.load());
     }
 }
 export namespace DuckDB {
-    export function load() {
+    export function load(): Promise<DuckDBTypes.DuckDB> {
         return import("./duckdb.js").then(mod => mod.DuckDB.load());
     }
 }
 export namespace Expat {
-    export function load() {
+    export function load(): Promise<ExpatTypes.Expat> {
         return import("./expat.js").then(mod => mod.Expat.load());
     }
 }
 export namespace Graphviz {
-    export function load() {
+    export function load(): Promise<GraphvizTypes.Graphviz> {
         return import("./graphviz.js").then(mod => mod.Graphviz.load());
     }
 }
 export namespace Zstd {
-    export function load() {
+    export function load(): Promise<ZstdTypes.Zstd> {
         return import("./zstd.js").then(mod => mod.Zstd.load());
     }
 }
