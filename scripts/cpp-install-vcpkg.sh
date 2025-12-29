81c6a288222f97026f7df013c17c9b3ee2894164
Gordon Smith <GordonJSmith@gmail.com>
2025-12-29 12:51:48 +0000

    fix: add binary caching to github runners
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- scripts/cpp-install-vcpkg.sh
+++ scripts/cpp-install-vcpkg.sh
@@ -12,5 +12,5 @@ cd ./vcpkg
 git checkout $VCPKG_BUILD_TOOLS_VERSION
 ./bootstrap-vcpkg.sh
 cd ..
-source ./emsdk/emsdk_env.sh
-./vcpkg/vcpkg install --triplet=wasm32-emscripten --x-abi-tools-use-exact-versions --downloads-root=./build/vcpkg_downloads --x-buildtrees-root=./build/vcpkg_buildtrees --x-packages-root=./build/vcpkg_packages --x-install-root=./build/vcpkg_installed
+# source ./emsdk/emsdk_env.sh
+# ./vcpkg/vcpkg install --triplet=wasm32-emscripten --x-abi-tools-use-exact-versions --downloads-root=./build/vcpkg_downloads --x-buildtrees-root=./build/vcpkg_buildtrees --x-packages-root=./build/vcpkg_packages --x-install-root=./build/vcpkg_installed
