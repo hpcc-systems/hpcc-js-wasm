afc2f98398ce25f8b5c5e4dfe7223264521653b9
Gordon Smith <GordonJSmith@gmail.com>
2026-01-27 08:20:23 +0000

    fix: bump versions (security)
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- scripts/cpp-install-vcpkg.sh
+++ scripts/cpp-install-vcpkg.sh
@@ -2,7 +2,7 @@
 
 # List of current version can be found in https://github.com/microsoft/vcpkg/releases  ---
 # UPDATE README.md
-VCPKG_BUILD_TOOLS_VERSION=2025.12.12
+VCPKG_BUILD_TOOLS_VERSION=2026.01.16
 
 if [ ! -d "./vcpkg" ] 
 then
